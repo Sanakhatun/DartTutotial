0a18e05b7c6621b6cbb61e3eff6b3bf6e042eb08
Sanakhatun Shaikh <sanakhatun.shaikh@mobicule.com>
2022-08-24 01:08:15 +0530

    String datatype implementation

--- main.dart
+++ main.dart
@@ -1,4 +1,6 @@
 void main() {
+  //NOTE: All data types in Dart are Objects.
+
   print("Hello World");
 
   //Number: int
@@ -58,5 +60,58 @@ void main() {
   const msUntilRetry = secondsUntilRetry * msPerSecond;
   print(msUntilRetry);
 
-  //NOTE: All data types in Dart are Objects.
+  //Strings
+  var s1 = 'Single quotes work well for string literals.';
+  print(s1);
+
+  var s2 = "Double quotes just work as well.";
+  print(s2);
+
+  var s3 = 'It\'s easy to escape the string delimiter.';  //use escape characters
+  print(s3);
+
+  var s4 = "It's even easier to use the other delimiter.";
+  print(s4);
+
+//  String interpolation
+  var s = "String interpolation";
+  print("Dart has $s, which is very handy.");  //Recommended and avoid concatenation with + symbol
+
+  String firstName = 'Sana';
+  print("The number of characters in String firstName is " + firstName.length.toString()); //Not Recommended
+
+  //Recommended as per Dart Coding Conventions
+  print("The number of characters in String firstName is ${firstName.length}");
+
+  int a = 10;
+  int b = 20;
+
+  print("Sum of $a and $b is ${a+b}");
+
+//String concatenation
+
+//  You can concatenate strings using adjacent string literals or the + operator:
+  var s5 = 'String '
+      'concatenation'
+      " works even over line breaks.";
+  print(s5);
+
+  var s6 = "Sana " + 'Shaikh';
+  print(s6);
+
+//Multiline String
+  var s7 = '''
+  You can create 
+  multiline string like this one
+  ''';
+  print(s7);
+
+  var s8 = """This is also a
+  multi-line string.""";
+  print(s8);
+
+  var s9 = '\n' r'In a raw string, not even \n gets special treatment';
+  print(s9);
+
+
 }
